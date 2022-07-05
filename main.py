5cddd6066bdb47dca6fc634d179bc1f61566c493
Kostenko Andriy <a.kostenkouk@gmail.com>
2022-07-05 11:51:09 +0300

    adding new functions:add,remove,show users

--- main.py
+++ main.py
@@ -14,18 +14,31 @@ users = [
      'age':14}
 ]
 
+
 class Users(BaseModel):
     name: str
     surname: str
     age: int
 
 
-
 @app.get("/")
-async def read_root():
-    return {'Hello': 'World'}
+async def all_users():
+    return {'users': users}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, 'q':q}
+@app.get("/add_user/")
+async def add_user(name: str, surname: str, age: int):
+    if {'name': name, 'surname': surname, 'age': age} in users:
+        print('Such user already exist.')
+    else:
+        users.append({'name': name, 'surname': surname, 'age': age})
+    return {'name': name, 'surname': surname, 'age': age}
+
+
+@app.get("/delete_user/")
+async def delete_user(name: str, surname: str, age: int):
+    if {'name': name, 'surname': surname, 'age': age} in users:
+        users.remove({'name': name, 'surname': surname, 'age': age})
+    else:
+        pass
+    return {'users': users}
