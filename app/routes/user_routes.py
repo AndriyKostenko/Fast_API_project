033f67bcf567400e23c6bcf614a54bf3108edbf7
Kostenko Andriy <a.kostenkouk@gmail.com>
2022-07-06 14:14:43 +0300

    refactoring the code to correct get/post/delete methods

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,5 +1,7 @@
 from fastapi import HTTPException
 from fastapi import APIRouter
+from models.user_models import User
+
 
 route = APIRouter(tags=['users'])
 
@@ -21,24 +23,25 @@ async def all_users():
 @route.get('/users/{user_id}')
 def get_user(user_id: int):
     try:
-        return {'user_info': users[user_id]}
+        return {'user_info': users[user_id-1]} # to get started from 1
     except IndexError:
-        raise HTTPException(status_code=404, detail='Not found')
+        raise HTTPException(status_code=404, detail='User not found')
 
 
 @route.post("/add_user/")
-async def add_user(name: str, surname: str, age: int):
-    if {'name': name, 'surname': surname, 'age': age} in users:
-        print('Such user already exist.')
+async def add_user(user: User):
+    print(user)
+    if user in users:
+        raise HTTPException(status_code=409, detail='Such user already exist.')
     else:
-        users.append({'name': name, 'surname': surname, 'age': age})
-    return {'name': name, 'surname': surname, 'age': age}
+        users.append(user)
+    return {'users': users}
 
 
-@route.get("/delete_user/")
-async def delete_user(name: str, surname: str, age: int):
-    if {'name': name, 'surname': surname, 'age': age} in users:
-        users.remove({'name': name, 'surname': surname, 'age': age})
+@route.delete("/delete_user/")
+async def delete_user(user: User):
+    if user in users:
+        users.remove(user)
     else:
-        print('Such user isn\'t exist.')
+        raise HTTPException(status_code=404, detail='User not found.')
     return {'users': users}
